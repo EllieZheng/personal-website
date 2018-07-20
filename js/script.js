0541aad61b6691d6834652beaec1e26ee886818c
Ellie Zheng <lianjunzheng.pku09@gmail.com>
2018-07-20 01:05:15 -0400

    update all htmls with common header/footer in script.js

--- js/script.js
+++ js/script.js
@@ -7,13 +7,60 @@ $( function(){ // == document.addEventListener("DOMContentLoaded"...)
 			}
 	});
 	
+	/* This is not working locally in Chrome....
 	// Load header
 	$(function(){
 		$("#header-common").load("common/header-common.html");
 		$("#footer-common").load("common/footer-common.html");
 	});
+	*/
 });
 
 (function (global) {
 
 })(window);
+
+document.getElementById("footer").innerHTML =
+  "<footer class='footer-panel'>"
+  + "<div class='container'>"
+  +   "&copy;Copyright Lianjun Zheng "+ new Date().getFullYear()
+  + "</div>"
+  +"</footer>";
+  
+document.getElementById("header").innerHTML =
+" <header> "
++ "    <nav id='header-nav' class='navbar navbar-default navbar-fixed-top'> "
++ "      <div class='container'> "
++ "        <div class='navbar-header'> "
++ "           <a class='navbar-brand' href='index.html'> "
++ "              <img id='logo' alt='Brand' src='image/logo.png'> "
++ "            </a> "
++ "                     "
++ "          <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#collapsable-nav' aria-expanded='false'> "
++ "            <span class='sr-only'>Toggle navigation</span> "
++ "            <span class='icon-bar'></span> "
++ "            <span class='icon-bar'></span> "
++ "            <span class='icon-bar'></span> "
++ "          </button> "
++ "        </div> "
++ "        <div id='collapsable-nav' class='collapse navbar-collapse'> "
++ "          <ul id='nav-list' class='nav navbar-nav navbar-right'> "
++ "            <li class='text-center'><a target='_blank' href='CV/CV_EllieZheng.pdf'>CV</a></li> "
++ "            <li class='text-center'><a href='research.html'>Research</a></li> "
++ "            <li class='text-center'><a target='_blank' href='https://sites.google.com/site/elliezheng91/home'>Teaching</a></li> "
++ "            <li class='text-center'><a target='_blank' href='https://www.linkedin.com/in/lianjun-zheng-2597a495/'>LinkedIn</a></li> "
++ "            <li class='text-center'><a href='app.html'>Apps</a></li> "
++ "            <!-- <li class='text-center'><a href='#'>About</a></li> --> "
++ "          </ul><!-- #nav-list --> "
++ "        </div><!-- .collapse .navbar-collapse --> "
++ "      </div> "
++ "    </nav> "
++ "</header> "
++ "<div class='container'> "
++ "      <img class='visible-xs visible-sm visible-md visible-lg' width='100%' src='image/head.jpg'> "
++ "      <img class='visible-xxs' width='100%' src='image/head-small.jpg'> "
++ "      <div id='head'> "
++ "        <h1 class='text-right'>Lianjun Zheng <span>(Ellie)</span></h1> "
++ "      </div> "
++ " </div>  ";
+
